bd573279cb4da12710a6993691c613982b55b0a4
Roby8301 <al396796@gmail.com>
2025-02-12 22:42:24 +0100

    Paso 1

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+export function App(){
+    return(
+      <article className='tw-followCard'>
+        <header className='tw-followCard-header'>
+            <img className='tw-followCard-avatar'
+            alt ="midudev" src="https://unavatar.io/midudev"/>
+            <div className='tw-followCard-info'>
+                <strong>Miguel Ángel Durán</strong>
+                <span className='tw-followCard-infoUserName'>@midudev</span>
+            </div>
+        <aside>
+            <button className='tw-followCard-button'>
+                Seguir
+            </button>
+        </aside>
+        </header>
+      </article>
+    )
+}
